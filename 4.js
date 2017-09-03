38abcd8d4855541f97963afe574ef2a5e09684b5
paaulinaaas <psikora089@gmail.com>
2017-09-03 09:56:27 +0100

    Update 4.js

--- 4.js
+++ 4.js
@@ -1,2 +1,4 @@
-const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1]
+const average = ((...num) => num.reduce((a,b) => a+b))(...grades) / grades.length
+console.log(average);
 
